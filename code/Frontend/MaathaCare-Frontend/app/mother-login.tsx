e364a756e7f203d6bddcbec2c20fe7d717de7440
Mahee004 <mahishamadubhashinie@gmail.com>
2026-03-06 11:39:03 +0530

    implement secure JWT data fetching on PHM dashboard

--- code/Frontend/MaathaCare-Frontend/app/mother-login.tsx
+++ code/Frontend/MaathaCare-Frontend/app/mother-login.tsx
@@ -44,7 +44,7 @@ export default function App() {
 
       // 2. Send the data to Spring Boot
       const response = await axios.post(
-        "http://172.20.10.2:8080/api/users/login",
+        "http://192.168.8.180:8080/api/users/login",
         {
           phoneNumber: phoneNumber,
           password: password,
